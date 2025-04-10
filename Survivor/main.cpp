4973c8827cf55ee10cca5cbb77c012173ce6093b
Ineverlucky <1271507759@qq.com>
2025-04-10 10:51:32 +0800

    导入角色动画，但是角色会闪动

--- Survivor/main.cpp
+++ Survivor/main.cpp
@@ -8,6 +8,15 @@ const int PLAYER_ANIM_NUM = 4; //动画帧总数常量
 IMAGE imgPlayerDown[PLAYER_ANIM_NUM];
 IMAGE imgPlayerUp[PLAYER_ANIM_NUM];
 
+#pragma comment(lib, "MSIMG32.LIB")
+
+inline void putimage_alpha(int x, int y, IMAGE* img)
+{
+	int w = img->getwidth();
+	int h = img->getheight();
+	AlphaBlend(GetImageHDC(NULL), x, y, w, h, GetImageHDC(img), 0, 0, w, h, { AC_SRC_OVER, 0, 255, AC_SRC_ALPHA });	
+}
+
 void LoadAnimation()
 {
 	for (size_t i = 0; i < PLAYER_ANIM_NUM; i++)
@@ -26,14 +35,13 @@ void LoadAnimation()
 int main()
 {
 	initgraph(1280, 720);
+	setbkcolor(RGB(144, 238, 144));//填充背景颜色
+	LoadAnimation();
 
 	bool running = true;
 	ExMessage msg;
 
 	BeginBatchDraw();
-
-	setbkcolor(RGB(144, 238, 144));//填充背景颜色
-
 
 	while (running)
 	{
@@ -45,20 +53,22 @@ int main()
 		}
 
 		static int counter = 0; //记录当前游戏帧一共播放了多少动画帧
-
-		if (++counter % 5 == 0) //每5个游戏帧播放一个动画帧
+		if (++counter %5 == 0) //每5个游戏帧播放一个动画帧
 			idxCurrentAnim++;
 
 		idxCurrentAnim = idxCurrentAnim % PLAYER_ANIM_NUM; //帧索引超过帧总数之后归零
 
 		cleardevice();
+
+		putimage_alpha(500, 500, &imgPlayerDown[idxCurrentAnim]);
+
 		FlushBatchDraw();
 
 		DWORD endTime = GetTickCount();
 		DWORD deltaTime = endTime - startTime;
-		if (deltaTime < 1000 / 144)
+		if (deltaTime < 1000 / 60)
 		{
-			Sleep(1000 / 144 - deltaTime);
+			Sleep(1000 / 60 - deltaTime);
 		}
 	}
 
